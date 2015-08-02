5d3d79f90e4d53723ccc95a8c970848b23432c8a
Martijn van de Wetering <vandewetering.m@gmail.com>
2015-08-02 19:55:40 +0200

    Initial commit

---
+++ PracticesAndPatterns.Patterns/Singleton/Singleton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticesAndPatterns.Patterns.Singleton
+{
+    public class Singleton
+    {
+        private static class SingletonHolder
+        { 
+            internal static readonly Singleton Instance = new Singleton();
+
+            // Empty static constructor - forces laziness.
+            static SingletonHolder()
+            {
+            }
+        }
+
+        // Internal class SingletonHolder can still call this private constructor.
+        private Singleton()
+        {
+        }
+
+        public static Singleton Instance
+        {
+            get { return SingletonHolder.Instance; }
+        }
+
+        public void DoSomething()
+        {
+            Console.WriteLine("Doing some work!");
+        }
+    }
+}
